0d12ecc7d0fd1d3ddc2f99dd21b358a099fa2d90
omkar <omkar.jejurkar@bito.ai>
2024-01-13 21:27:03 +0530

    initial commit

---
+++ src/main/java/com/demo/websocket/controller/TestController.java
@@ -0,0 +1,44 @@
+package com.demo.websocket.controller;
+
+import com.demo.websocket.model.MyMessage;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+@Controller
+public class TestController {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    TestController(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/receive")
+    public MyMessage sendMessage(MyMessage myMessage){
+        System.out.println(myMessage.getContent());
+        System.out.println(myMessage.getHr());
+        System.out.println(myMessage.getMin());
+        return myMessage;
+    }
+
+    @MessageMapping("/sendMessage/{userId}")
+//    @SendTo("/topic/receive/1")
+    public void sendPersonalMessage(@DestinationVariable int userId, MyMessage myMessage){
+        System.out.println(myMessage.getContent());
+        System.out.println(myMessage.getHr());
+        System.out.println(myMessage.getMin());
+        String userDestination = "/topic/receive/"+userId;
+        simpMessagingTemplate.convertAndSend(userDestination, myMessage);
+//        return myMessage;
+    }
+
+    @GetMapping("/personal")
+    public String getIndex1(){
+        return "index1.html";
+    }
+
+}
---
+++ src/main/java/com/demo/websocket/model/MyMessage.java
@@ -0,0 +1,13 @@
+package com.demo.websocket.model;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class MyMessage {
+    private String senderName;
+    private String content;
+    private int hr;
+    private int min;
+}
